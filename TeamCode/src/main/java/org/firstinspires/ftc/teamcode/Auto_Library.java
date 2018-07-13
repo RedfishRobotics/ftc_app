d6d093d93b1a18fbe4b3514f252558b70abd41e7
CameronClark <futureengr2025@gmail.com>
2018-07-13 11:49:45 -0500

    Auto Library added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Library.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Auto_Library extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+
+    }
+}
