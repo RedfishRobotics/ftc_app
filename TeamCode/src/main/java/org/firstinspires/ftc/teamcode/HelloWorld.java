c1314f8176da6264cf2cf6fc03e6a1b7679562fd
CameronClark <futureengr2025@gmail.com>
2018-07-13 16:21:05 -0500

    First running test if the library (Teleop)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Library Test", group="Linear Opmode")
 
@@ -10,10 +12,65 @@ public class HelloWorld extends LinearOpMode {
     Auto_Library autoLibrary = new Auto_Library();
     TeleOp_Library teleOpLibrary = new TeleOp_Library();
 
+
     @Override
     public void runOpMode() {
-        teleOpLibrary.driveAndStrafe();
-        //This is a test of Git
-        //Another test
+        //autoLibrary.init(hardwareMap);
+        teleOpLibrary.init(hardwareMap);
+        waitForStart();
+//            autoLibrary.encoderDrive(0.5, 25, 25, 5);
+//            autoLibrary.rightFrontDrive.setPower(0.5);
+//            sleep(1000);
+//            autoLibrary.rightFrontDrive.setPower(0.0);
+        while (opModeIsActive()) {
+            if (gamepad1.right_bumper) {
+                teleOpLibrary.runIntakeForward();
+            }
+            if (gamepad1.left_bumper) {
+                teleOpLibrary.stopIntake();
+            }
+            if (gamepad1.dpad_down) {
+                teleOpLibrary.runIntakeHalf();
+            }
+            if (gamepad1.dpad_up) {
+                teleOpLibrary.runIntakeBackward();
+            }
+            if (gamepad2.left_bumper) {
+                teleOpLibrary.relicClawClose();
+            }
+            if (gamepad2.right_bumper) {
+                teleOpLibrary.relicClawOpen();
+            }
+            if (gamepad2.dpad_down) {
+                teleOpLibrary.pivotRelicDown();
+            }
+            if (gamepad2.dpad_up) {
+                teleOpLibrary.pivotRelicUp();
+            }
+            if (gamepad2.y) {
+                teleOpLibrary.deployRelic();
+            }
+            if (gamepad2.b) {
+                teleOpLibrary.flipperFlatwithStop();
+            }
+            if (gamepad1.x) {
+                teleOpLibrary.flipperFlat();
+            }
+            if (gamepad1.b && teleOpLibrary.leftFlip.getPosition() >= 0.15) {
+                teleOpLibrary.flipperDump();
+            }
+            if (gamepad1.a && teleOpLibrary.LeftLiftMotor.getCurrentPosition() < 20) {
+                teleOpLibrary.flipperDown();
+            }
+            if (gamepad2.a) {
+                teleOpLibrary.lifterDown();
+            }
+            if (gamepad2.x && teleOpLibrary.leftFlip.getPosition() >= 0.15) {
+                teleOpLibrary.lifterUp();
+            }
+//            //teleOpLibrary.telemetry();
+//            //This is a test of Git
+//            //Another test
+        }
     }
 }
