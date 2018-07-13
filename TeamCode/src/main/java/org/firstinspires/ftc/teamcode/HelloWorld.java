0455c158de8a148714e98b5753a72822e7f51fbb
CameronClark <futureengr2025@gmail.com>
2018-07-13 12:45:01 -0500

    cleaned the inport blocks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Library Test", group="Linear Opmode")
 
