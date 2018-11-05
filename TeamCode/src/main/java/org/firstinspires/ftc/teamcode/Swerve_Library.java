2f199f0c10b93145ff8be994ccf212837a0c45e0
CameronClark <futureengr2025@gmail.com>
2018-11-05 11:11:18 -0600

    Added a teleOp Swerve Library to hold the config and the Swerve Turn Positions. Will expand as we finalise the design

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve_Library.java
@@ -0,0 +1,151 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Swerve_Library extends LinearOpMode {
+
+    public ElapsedTime runtime = new ElapsedTime();
+    public Servo SwervePod1 = null;
+    public Servo SwervePod2 = null;
+    public Servo SwervePod3 = null;
+    public Servo SwervePod4 = null;
+    public Servo ScoreFlipper = null;
+    public Servo cubeKnockRight = null;
+    public  Servo cubeKnockLeft = null;
+    public DcMotor IntakeLiftRight = null;
+    public DcMotor IntakeLiftLeft = null;
+    public DcMotor SwervePod1motor = null;
+    public DcMotor SwervePod2motor = null;
+    public DcMotor SwervePod3motor = null;
+    public DcMotor SwervePod4motor = null;
+    public DcMotor IntakeMotor = null;
+    public DcMotor LiftMotor = null;
+
+    private static final double[] Latch = new double[]{0, 1};
+    private int currentLatchIndex;
+
+    HardwareMap hardwareMap           =  null;
+
+
+    @Override
+    public void runOpMode(){
+
+    }
+
+    public void init(HardwareMap ahwMap){
+        hardwareMap = ahwMap;
+
+        this.currentLatchIndex = 0;
+
+        SwervePod1 = hardwareMap.get(Servo.class, "Swerve_Pod1");
+        SwervePod2 = hardwareMap.get(Servo.class, "Swerve_Pod2");
+        SwervePod3 = hardwareMap.get(Servo.class, "Swerve_Pod3");
+        SwervePod4 = hardwareMap.get(Servo.class, "Swerve_Pod4");
+        cubeKnockLeft = hardwareMap.get(Servo.class, "knockleft");
+        cubeKnockRight = hardwareMap.get(Servo.class, "knockright");
+        ScoreFlipper = hardwareMap.get(Servo.class, "ScoreFlipper");
+//        IntakeSlide = hardwareMap.get(DcMotor.class, "IntakeSlide");
+
+        SwervePod1motor = hardwareMap.get(DcMotor.class, "Swerve_Pod1motor");
+        SwervePod2motor = hardwareMap.get(DcMotor.class, "Swerve_Pod2motor");
+        SwervePod3motor = hardwareMap.get(DcMotor.class, "Swerve_Pod3motor");
+        SwervePod4motor = hardwareMap.get(DcMotor.class, "Swerve_Pod4motor");
+        IntakeLiftLeft = hardwareMap.get(DcMotor.class, "IntakeLiftLeft");
+        IntakeLiftRight = hardwareMap.get(DcMotor.class, "IntakeLiftRight");
+        IntakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
+        LiftMotor = hardwareMap.get(DcMotor.class, "LiftMotor");
+
+        SwervePod1motor.setDirection(DcMotor.Direction.REVERSE);
+        SwervePod2motor.setDirection(DcMotor.Direction.REVERSE);
+        SwervePod3motor.setDirection(DcMotor.Direction.FORWARD);
+        SwervePod4motor.setDirection(DcMotor.Direction.FORWARD);
+        IntakeLiftRight.setDirection(DcMotor.Direction.REVERSE);
+        IntakeLiftLeft.setDirection(DcMotor.Direction.FORWARD);
+        IntakeMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        SwervePod1motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        SwervePod2motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        SwervePod3motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        SwervePod4motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeLiftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        IntakeLiftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        SwervePod1motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        SwervePod2motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        SwervePod3motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        SwervePod4motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        IntakeLiftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        IntakeLiftLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        if (currentLatchIndex == 0) {
+            SwervePod1motor.setDirection(DcMotor.Direction.REVERSE);
+            SwervePod2motor.setDirection(DcMotor.Direction.REVERSE);
+            SwervePod3motor.setDirection(DcMotor.Direction.FORWARD);
+            SwervePod4motor.setDirection(DcMotor.Direction.FORWARD);
+        } else if (currentLatchIndex == 1) {
+            SwervePod1motor.setDirection(DcMotor.Direction.REVERSE);
+            SwervePod2motor.setDirection(DcMotor.Direction.FORWARD);//changed
+            SwervePod3motor.setDirection(DcMotor.Direction.FORWARD);
+            SwervePod4motor.setDirection(DcMotor.Direction.REVERSE);//changed
+        }
+
+        double SwervePod1Position = 0.5;
+        double SwervePod2Position = 0.5;
+        double SwervePod3Position = 0.5;
+        double SwervePod4Position = 0.5;
+        double CubeKnockLeftPosition = 0.92;
+        double CubeKnockRightPosition = 0.13;
+        double ScoreFlipperPosition = 0.2;
+        ScoreFlipper.setPosition(ScoreFlipperPosition);
+        SwervePod2.setPosition(SwervePod2Position);
+        SwervePod1.setPosition(SwervePod1Position);
+        SwervePod3.setPosition(SwervePod3Position);
+        SwervePod4.setPosition(SwervePod4Position);
+        cubeKnockRight.setPosition(CubeKnockRightPosition);
+        cubeKnockLeft.setPosition(CubeKnockLeftPosition);
+        //IntakeSlide.setPosition(IntakePosition);
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");
+
+    }
+    public void straightPosition(){
+        SwervePod1.setPosition(0.5);
+        SwervePod2.setPosition(0.5);
+        SwervePod3.setPosition(0.5);
+        SwervePod4.setPosition(0.5);
+        currentLatchIndex = 0 % Latch.length;
+    }
+    public void turnPosition(){
+        SwervePod1.setPosition(0.8);
+        SwervePod2.setPosition(0.2);
+        SwervePod3.setPosition(0.8);
+        SwervePod4.setPosition(0.2);
+        currentLatchIndex = 1 % Latch.length;
+    }
+    public void spinPosition(){
+        SwervePod1.setPosition(0.68);
+        SwervePod2.setPosition(0.35);
+        SwervePod3.setPosition(0.68);
+        SwervePod4.setPosition(0.35);
+        currentLatchIndex = 0 % Latch.length;
+    }
+    public void right45Position(){
+        SwervePod1.setPosition(0.35);
+        SwervePod2.setPosition(0.35);
+        SwervePod3.setPosition(0.35);
+        SwervePod4.setPosition(0.35);
+        currentLatchIndex = 0 % Latch.length;
+    }
+    public void left45Position(){
+        SwervePod1.setPosition(0.685);
+        SwervePod2.setPosition(0.68);
+        SwervePod3.setPosition(0.68);
+        SwervePod4.setPosition(0.685);
+        currentLatchIndex = 0 % Latch.length;
+    }
+
+}
