9d73ae65d405bcdcf7f5d604cd057a75f9c79708
CameronClark <futureengr2025@gmail.com>
2019-01-31 13:34:01 -0600

    Changed the TeleOp that we are using to run on the library

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideLiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideLiftTest.java
@@ -5,89 +5,109 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
 
-@TeleOp(name="IntakeLiftTest", group="Pushbot")
+
+@TeleOp(name="NewRobotTest", group="Pushbot")
 public class SlideLiftTest extends LinearOpMode {
 
-    public DcMotor liftSlideRight = null;
-    public DcMotor liftSlideLeft = null;
-    public DigitalChannel magneticSwitch = null;
-    public CRServo flipperServo = null;
+    TeleOp_Library_Rover_Ruckus TeleOp = new TeleOp_Library_Rover_Ruckus();
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
 
-        liftSlideLeft = hardwareMap.get(DcMotor.class, "Lift_Slide_Left");
-        liftSlideRight = hardwareMap.get(DcMotor.class, "Lift_Slide_Right");
-        magneticSwitch = hardwareMap.get(DigitalChannel.class, "Hull_Effect");
-        flipperServo = hardwareMap.get(CRServo.class, "Flip_Servo");
-
-        liftSlideRight.setDirection(DcMotor.Direction.REVERSE);
-        liftSlideLeft.setDirection(DcMotor.Direction.REVERSE);
-
-        liftSlideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftSlideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-//        liftSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        liftSlideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
+        TeleOp.init(hardwareMap);
         waitForStart();
 
         while (opModeIsActive()){
-//            if (magneticSwitch.getState() == true) {
-//                telemetry.addData("Magnetic Switch", "Is Opened");
-//                flipperServo.setPower(0.5);
-//            } else {
-//                telemetry.addData("Magnetic Switch", "Is Closed");
-//                flipperServo.setPower(0);
-//            }
-
+            if(gamepad1.a){
+                TeleOp.intakeIn();
+            }
+            if(gamepad1.b){
+                TeleOp.intakeStop();
+            }
+            if(gamepad1.x){
+                TeleOp.intakeOut();
+            }
+            if(gamepad1.dpad_down){
+                TeleOp.lifterTucked();
+            }
+            if(gamepad1.dpad_right){
+                TeleOp.lifterScoring();
+            }
+            if(gamepad1.dpad_up){
+                TeleOp.lifterDown();
+            }
+            if (gamepad1.left_trigger > 0.2) {
+                TeleOp.leftFrontDrive.setPower(gamepad1.left_trigger);
+                TeleOp.leftRearDrive.setPower(-gamepad1.left_trigger);
+                TeleOp.rightFrontDrive.setPower(-gamepad1.left_trigger);
+                TeleOp.rightRearDrive.setPower(gamepad1.left_trigger);
+            } if (gamepad1.right_trigger > 0.2) {
+                TeleOp.leftFrontDrive.setPower(-gamepad1.right_trigger);
+                TeleOp.leftRearDrive.setPower(gamepad1.right_trigger);
+                TeleOp.rightFrontDrive.setPower(gamepad1.right_trigger);
+                TeleOp.rightRearDrive.setPower(-gamepad1.right_trigger);
+            } else {
+                TeleOp.leftFrontDrive.setPower(-gamepad1.left_stick_y);
+                TeleOp.leftRearDrive.setPower(-gamepad1.left_stick_y);
+                TeleOp.rightFrontDrive.setPower(-gamepad1.right_stick_y);
+                TeleOp.rightRearDrive.setPower(-gamepad1.right_stick_y);
+            }
+            RobotLog.i("right lift position "+ TeleOp.rightLifter.getCurrentPosition());
+            RobotLog.i("left lift position " + TeleOp.leftLifter.getCurrentPosition());
+            telemetry.addLine("Left lift position: " + TeleOp.leftLifter.getCurrentPosition());
+            telemetry.addLine("Right lift position: "+ TeleOp.rightLifter.getCurrentPosition());
             telemetry.update();
 
-//            if(gamepad1.y){
+        }
+    }
+//public void liftTestRight(int target){
+//    //Sets the new target position for the glyph lifter
+//    // RightLiftMotor.setTargetPosition(target);
+//    rightLifter.setTargetPosition(target);
+//    //Turns on RUN_TO_POSITION
+//    // RightLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//    rightLifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//    //If statement that ask if the motor is busy
+//    if( rightLifter.isBusy()){
+//        //If statement that checks if the motors current position is more then the target
+//        if( rightLifter.getCurrentPosition() > target){
+//            //If the current position is more than the target, set motor power to 40%
+//            // RightLiftMotor.setPower(0.45);
+//            rightLifter.setPower(0.4);
+//        }
+//        //If statement that checks if the motors current position is less then the target
+//        else if( rightLifter.getCurrentPosition() < target){
+//            //If the current position is more than the target, set motor power to 60%
+//            // RightLiftMotor.setPower(0.5);
+//            rightLifter.setPower(0.15);
 //
+//        }
+//    }
+//}
+//    public void liftTestLeft(int target){
+//        //Sets the new target position for the glyph lifter
+//        // RightLiftMotor.setTargetPosition(target);
+//        leftLifter.setTargetPosition(target);
+//        //Turns on RUN_TO_POSITION
+//        // RightLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        leftLifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        //If statement that ask if the motor is busy
+//        if( leftLifter.isBusy()){
+//            //If statement that checks if the motors current position is more then the target
+//            if( leftLifter.getCurrentPosition() > target){
+//                //If the current position is more than the target, set motor power to 40%
+//                // RightLiftMotor.setPower(0.45);
+//                leftLifter.setPower(0.4);
 //            }
-//            if(gamepad1.x){
-//                LiftTest(0);
+//            //If statement that checks if the motors current position is less then the target
+//            else if( leftLifter.getCurrentPosition() < target){
+//                //If the current position is more than the target, set motor power to 60%
+//                // RightLiftMotor.setPower(0.5);
+//                leftLifter.setPower(0.15);
 //            }
-//            if(gamepad1.b){
-//                LiftTest(3600);
-//                sleep(2000);
-//                while(magneticSwitch.getState() == true){
-//                    flipperServo.setPower(0.75);
-//                }
-//                flipperServo.setPower(0);
-//            }
-            liftSlideLeft.setPower(-gamepad1.right_stick_y);
-            liftSlideRight.setPower(-gamepad1.right_stick_y);
-        }
-    }
-    public void LiftTest(int target){
-        //Sets the new target position for the glyph lifter
-        // RightLiftMotor.setTargetPosition(target);
-        liftSlideLeft.setTargetPosition(target);
-        liftSlideRight.setTargetPosition(target);
-        //Turns on RUN_TO_POSITION
-        // RightLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftSlideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //If statement that ask if the motor is busy
-        if( liftSlideLeft.isBusy()){
-            //If statement that checks if the motors current position is more then the target
-            if( liftSlideLeft.getCurrentPosition() > target){
-                //If the current position is more than the target, set motor power to 40%
-                // RightLiftMotor.setPower(0.45);
-                liftSlideLeft.setPower(0.25);
-                liftSlideRight.setPower(0.25);
-            }
-            //If statement that checks if the motors current position is less then the target
-            else if( liftSlideLeft.getCurrentPosition() < target){
-                //If the current position is more than the target, set motor power to 60%
-                // RightLiftMotor.setPower(0.5);
-                liftSlideLeft.setPower(0.75);
-                liftSlideRight.setPower(0.75);
-            }
-        }
-    }
+//        }
+//    }
 }
